4e1b356a268a774b9710a5f250b54976dac72faa
fairflow <matthew@fairtlough.net>
2025-12-10 14:42:35 +0000

    Bump connection_monitor version to 2.1.0 - bootstrap connection architecture

--- src/connection_monitor.py
+++ src/connection_monitor.py
@@ -12,10 +12,10 @@ Features:
 - Health monitoring and cleanup
 
 Author: Miolingo Team
-Version: 2.0.0
+Version: 2.1.0
 """
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 import streamlit as st
 import mysql.connector
